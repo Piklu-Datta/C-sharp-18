2980dd3d0c5d0ed948dca631e6c57bb560ae6685
Piklu Datta <pikludatta98@gmail.com>
2025-09-14 21:48:33 +0600

    Reflection first commit

---
+++ src/CodeSolution/Reflection/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        
+        public void displayName()
+        {
+            Console.WriteLine($"name is {Name}");
+        }
+
+    }
+}
---
+++ src/CodeSolution/Reflection/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+using Reflection;
+
+class Pogram
+{
+    static void Main(string[] args)
+    {
+        Type type = typeof(Person);
+        Console.WriteLine($"Type: {type.Name}");
+
+        Console.WriteLine("Properties");
+        
+        var properties = type.GetProperties();
+
+        foreach(var property in properties)
+        {
+            Console.WriteLine(property.Name);
+        }
+
+        Console.WriteLine("Methods");
+        var methods =type.GetMethods();
+
+        foreach(var method in methods)
+        {
+            Console.WriteLine(method.Name);
+        }
+    }
+}
