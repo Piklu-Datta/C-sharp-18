c64f207928ee7d56ea4b8af93279a515ef7fcd24
Piklu Datta <pikludatta98@gmail.com>
2025-09-14 22:15:59 +0600

    Refelection second commit

--- src/CodeSolution/Reflection/Program.cs
+++ src/CodeSolution/Reflection/Program.cs
@@ -6,24 +6,34 @@ class Pogram
 {
     static void Main(string[] args)
     {
-        Type type = typeof(Person);
-        Console.WriteLine($"Type: {type.Name}");
+        //Type type = typeof(Person);
+        //Console.WriteLine($"Type: {type.Name}");
 
-        Console.WriteLine("Properties");
-        
-        var properties = type.GetProperties();
+        //Console.WriteLine("Properties");
 
-        foreach(var property in properties)
-        {
-            Console.WriteLine(property.Name);
-        }
+        //var properties = type.GetProperties();
 
-        Console.WriteLine("Methods");
-        var methods =type.GetMethods();
+        //foreach(var property in properties)
+        //{
+        //    Console.WriteLine(property.Name);
+        //}
 
-        foreach(var method in methods)
-        {
-            Console.WriteLine(method.Name);
-        }
+        //Console.WriteLine("Methods");
+        //var methods =type.GetMethods();
+
+        //foreach(var method in methods)
+        //{
+        //    Console.WriteLine(method.Name);
+        //}
+
+        Type type = typeof(Person2);
+
+        object obj = Activator.CreateInstance(type,"Alice",100);
+
+        var name = type.GetProperty("Name").GetValue(obj);
+
+        var age = type.GetProperty("Id").GetValue(obj);
+
+        Console.WriteLine($"Name: {name} Age: {age}");
     }
 }
